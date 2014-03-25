b110e758b65d8391b6226acd5b6e961ac31c95a6
wy315700 <wy315700@gmail.com>
2014-03-26 01:28:06 +0800

    可以传输基本的内容
    可以传输html文件
    可以传输二进制文件了

--- pyHttp/main.py
+++ pyHttp/main.py
@@ -2,6 +2,8 @@
 import sys
 import socket
 import time
+import os
+import struct
 
 try:
     import gevent
@@ -12,6 +14,10 @@ except Exception, e:
     raise e
 
 BUFFER_SIZE = 4096
+
+BASE_HTML_DIR = 'C:\\Users\\wangyang\\Downloads\\lame3.99.5'
+
+BASE_HTML_FILE = 'index.html'
 
 def server(port):
     s = socket.socket()
@@ -30,18 +36,72 @@ def handle_request(s, sleep):
             data += tmp
             if len(tmp) < BUFFER_SIZE or len(tmp) == 0:
                 break
-
-        s.send('''http/1.0 200 OK
-
-                  Hello World! this is xiaorui.cc !!!\r\n''')
         print data
+        head_list = get_header_from_request(data)
+
+        head = head_list[0]
+
+        method = get_request_method(head)
+
+        path   = get_request_path(head)
+
+
+
+        s.send("http/1.0 200 OK\r\n\r\n")
+
+        for buff in read_html_from_file(path):
+            result = s.send(buff)
+            print result
+        
         # s.send(request_string)
         s.shutdown(socket.SHUT_WR)
         print '.','be killed'
     except Exception, ex:
         print ex
-    finally:
-                                                                                                                                                                                                                                                                                                                                                                                   
+    finally:                                                                                                                                                                                                                                                                                                                                                                  
         s.close()
+
+def get_header_from_request(request):
+    req_list = request.split("\r\n")
+    return req_list
+
+def get_request_method(req):
+    list = req.split(' ')
+    return list[0]
+
+def get_request_path(req):
+    list = req.split(' ')
+    return list[1]
+
+def read_html_from_file(url_path):
+
+    if is_path_a_dir(url_path):
+        url_path += BASE_HTML_FILE
+
+    file_path = BASE_HTML_DIR + url_path
+
+    file_path = os.path.normpath(file_path)
+    
+    print file_path
+    if not os.path.exists(file_path):
+        # return 0
+        pass
+
+
+    with open(file_path,'rb') as html_file:
+        line = html_file.read(2048)
+        while len(line) > 0:
+            yield line
+            line = html_file.read(2048)
+
+def is_path_a_dir(path):
+    if path[-1] == '/':
+        return True
+    else:
+        return False
+
+def phrase_other_header(request_list):
+    pass
+
 if __name__ == '__main__':
     server(7777)
